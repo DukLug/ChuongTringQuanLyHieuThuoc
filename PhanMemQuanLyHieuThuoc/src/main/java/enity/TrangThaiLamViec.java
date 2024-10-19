7d1e4ebe670e2b121b7af573d15c3713724f10d3
Hachiman <nguyentrungquyen2412@gmail.com>
2024-10-19 21:49:06 +0700

    dao vs entity khuyenmai va nhanvien

---
+++ PhanMemQuanLyHieuThuoc/src/main/java/enity/TrangThaiLamViec.java
@@ -0,0 +1,19 @@
+package enity;
+
+public enum TrangThaiLamViec {
+DangLam("Đang làm"),DaNghiViec("Đã nghỉ việc");
+	
+	private String trangThai;
+	
+	
+	TrangThaiLamViec(String trangThai) {
+			this.trangThai = trangThai; 
+	}
+	public String getTrangThai() {
+		return trangThai;
+	}		
+	@Override
+	public String toString() {
+		return this.trangThai;
+	}
+}
